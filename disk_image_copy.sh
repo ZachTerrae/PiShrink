cbe4f7094808d3560f6234a5cd39a004d7a19f4c
ZachTerrae <35944815+ZachTerrae@users.noreply.github.com>
2019-12-10 10:00:11 -0600

    Copy it back

--- disk_image_copy.sh
+++ disk_image_copy.sh
@@ -3,4 +3,7 @@ sudo umount /dev/sdb1
 sudo umount /dev/sdb2
 sudo dd if=/dev/sdb of=ct-client.img
 sudo pishrink.sh -d ct-client.img ct-client-shrunk.img
-#sudo kill -USR1 $(pgrep ^dd)
+#sudo kill -USR1 $(pgrep ^dd) #check on dd command progress
+
+#copy the shrunk image to a new sd card
+#dd if=ct-client-shrunk.img of=/dev/sdb
